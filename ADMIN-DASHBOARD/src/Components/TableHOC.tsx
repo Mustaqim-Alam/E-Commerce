ac855dcc6123dad217fa47edd11081cfb67e9d63
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-07-07 16:47:48 +0530

    Table Making

--- ADMIN-DASHBOARD/src/Components/TableHOC.tsx
+++ ADMIN-DASHBOARD/src/Components/TableHOC.tsx
@@ -1,11 +1,38 @@
-import { useTable, Column } from "react-table";
+import { useTable, Column, TableOptions } from "react-table";
+
 function TableHOC<T extends Object>(
-  data T[],
-  colums: <T> Column[],
+  data: T[],
+  columns: Column<T>[],
   containerClassName: string,
   heading: string
 ) {
-  return function HOC() {};
+  return function HOC() {
+    const options: TableOptions<T> = {
+      columns,
+      data,
+    };
+
+    const { getTableProps, getTableBodyProps, rows, headerGroups, prepareRow } =
+      useTable(options);
+
+    return (
+      <div className={containerClassName}>
+        <h2 className="heading">{heading}</h2>
+        <table>
+          <thead>
+            {headerGroups.map((headerGroups) => (
+              <tr {...headerGroups.getHeaderGroupProps()}>
+                {headerGroups.headers.map((column) => (
+                  <th {...column.getHeaderProps}>{column.render("herader")}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    );
+  };
 }
 
 export default TableHOC;
